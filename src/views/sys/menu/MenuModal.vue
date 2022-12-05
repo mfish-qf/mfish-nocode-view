8eb3bc6113c10aaf4c5abe1e3244b9b8470c7826
qiufeng <qiufeng9862@qq.com>
2022-12-05 22:44:17 +0800

    fix: 解决菜单初始化menuType不正确问题

--- src/views/sys/menu/MenuModal.vue
+++ src/views/sys/menu/MenuModal.vue
@@ -33,7 +33,7 @@ export default defineComponent({
           ...data.record
         }).then();
       }
-      setTreeData({ menuType: 0 }).then();
+      setTreeData({ menuType: data.record.menuType }).then();
     });
     const getTitle = computed(() => (!unref(isUpdate) ? "新增菜单" : "编辑菜单"));
 
