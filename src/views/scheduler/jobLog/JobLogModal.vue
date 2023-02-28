bf4de95c760fffba4cb5ab73ea551a64a7dc09af
qiufeng <qiufeng9862@qq.com>
2023-02-28 21:39:10 +0800

    feat: 调度日志完善

--- src/views/scheduler/jobLog/JobLogModal.vue
+++ src/views/scheduler/jobLog/JobLogModal.vue
@@ -1,70 +1,42 @@
 <!--
- @description: 任务日志
+ @description: 系统日志
  @author: mfish
- @date: 2023-02-14
+ @date: 2023-01-08
  @version: V1.0.0
 -->
 <template>
-  <BasicModal v-bind="$attrs" @register="registerModal" :title="getTitle" @ok="handleSubmit">
-    <BasicForm @register="registerForm" @submit="handleSubmit" />
+  <BasicModal v-bind="$attrs" @register="registerModal" title="查看任务日志" :showOkBtn="false" cancelText="关闭">
+    <Description @register="registerDesc" />
   </BasicModal>
 </template>
 <script lang="ts">
-import { ref, computed, unref } from "vue";
-import { BasicForm, useForm } from "/@/components/general/Form/index";
-import { jobLogFormSchema } from "./jobLog.data";
+import { ref } from "vue";
 import { BasicModal, useModalInner } from "/@/components/general/Modal";
-import { insertJobLog, updateJobLog } from "/@/api/scheduler/JobLog";
+import { Description, useDescription } from "/@/components/general/Description";
+import { jobLogSchema } from "/@/views/scheduler/jobLog/jobLog.data";
 
 export default {
   name: "JobLogModal",
-  components: { BasicModal, BasicForm },
+  components: { BasicModal, Description },
   emits: ["success", "register"],
-  setup(_, { emit }) {
-    const isUpdate = ref(true);
-    const [registerForm, { resetFields, setFieldsValue, validate }] = useForm({
-      labelWidth: 100,
-      baseColProps: { span: 12 },
-      schemas: jobLogFormSchema,
-      showActionButtonGroup: false,
-      autoSubmitOnEnter: true
-    });
+  setup() {
+    let jobLogData = ref();
     const [registerModal, { setModalProps, closeModal }] = useModalInner(async (data) => {
-      resetFields().then();
       setModalProps({ confirmLoading: false, width: "40%" });
-      isUpdate.value = !!data?.isUpdate;
-      if (unref(isUpdate)) {
-        setFieldsValue({
-          ...data.record
-        }).then();
-      }
+      jobLogData.value = { ...data };
     });
-    const getTitle = computed(() => (!unref(isUpdate) ? "新增任务日志" : "编辑任务日志"));
-
-    async function handleSubmit() {
-      let values = await validate();
-      setModalProps({ confirmLoading: true });
-      if (unref(isUpdate)) {
-        saveJobLog(updateJobLog, values);
-      } else {
-        saveJobLog(insertJobLog, values);
-      }
-    }
-
-    function saveJobLog(save, values) {
-      save(values).then(() => {
-        emit("success");
-        closeModal();
-      }).finally(() => {
-        setModalProps({ confirmLoading: false });
-      });
-    }
+    const [registerDesc] = useDescription({
+      data: jobLogData,
+      schema: jobLogSchema,
+      column: 2
+    });
 
     return {
+      registerDesc,
       registerModal,
-      registerForm,
-      getTitle,
-      handleSubmit
+      closeModal,
+      jobLogSchema,
+      jobLogData
     };
   }
 };
