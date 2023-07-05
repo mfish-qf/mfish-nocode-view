4c2ed56926e6677dc428dbffbd4b2b3bda72ddf8
qiufeng <qiufeng9862@qq.com>
2023-07-05 11:21:37 +0800

    feat: 成员管理优化

--- src/views/sys/account/index.vue
+++ src/views/sys/account/index.vue
@@ -11,6 +11,9 @@
         <a-button type="primary" @click="handleCreate" v-if="$props.source !== 1 && hasPermission('sys:account:insert')"
           >新增账号
         </a-button>
+        <a-button type="primary" @click="handleSelectAccount" v-if="$props.source === 1 && hasTenant()"
+          >添加成员
+        </a-button>
       </template>
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
@@ -41,6 +44,17 @@
                 tooltip: '重置密码',
                 onClick: handleChangePwd.bind(null, record.id),
                 ifShow: hasRole(SUPER_ROLE) && $props.source !== 1
+              },
+              {
+                icon: 'ant-design:minus-circle-outlined',
+                color: 'error',
+                tooltip: '移除成员',
+                popConfirm: {
+                  title: '是否确认移除',
+                  placement: 'left',
+                  confirm: handleRemoveUser.bind(null, record)
+                },
+                ifShow: $props.source === 1 && hasTenant()
               }
             ]"
           />
@@ -48,6 +62,7 @@
       </template>
     </BasicTable>
     <AccountModal @register="registerModal" @success="handleSuccess" :source="$props.source" />
+    <AccountSelectModal @register="registerSelectModal" @success="handleSuccess" :org-id="searchInfo.orgId" />
     <PasswordModal @register="registerPwdModal" />
   </PageWrapper>
 </template>
@@ -62,12 +77,15 @@
   import { columns, searchFormSchema } from "./account.data";
   import { usePermission } from "/@/hooks/web/UsePermission";
   import PasswordModal from "/@/views/sys/account/PasswordModal.vue";
-  import { getTenantUserList } from "/@/api/sys/SsoTenant";
+  import { deleteUserOrg, getTenantUserList } from "/@/api/sys/SsoTenant";
   import { useDesign } from "/@/hooks/web/UseDesign";
+  import AccountSelectModal from "/@/views/sys/account/AccountSelectModal.vue";
+  import { SsoUser } from "/@/api/sys/model/UserModel";
+  import { useMessage } from "/@/hooks/web/UseMessage";
 
   export default {
     name: "AccountManagement",
-    components: { PasswordModal, BasicTable, PageWrapper, OrgTree, AccountModal, TableAction },
+    components: { AccountSelectModal, PasswordModal, BasicTable, PageWrapper, OrgTree, AccountModal, TableAction },
     props: {
       source: {
         type: Number,
@@ -75,21 +93,26 @@
       }
     },
     setup(props) {
-      const { hasPermission, hasRole, SUPER_ROLE } = usePermission();
+      const { hasPermission, hasRole, SUPER_ROLE, hasTenant } = usePermission();
       const [registerModal, { openModal }] = useModal();
+      const [registerSelectModal, { openModal: openSelectModal }] = useModal();
       const [registerPwdModal, { openModal: openPwdModal }] = useModal();
+      const { createMessage } = useMessage();
       const searchInfo = reactive<Recordable>({});
       const { prefixCls } = useDesign("account");
       const api = ref();
+      let newColumns;
       if (props.source === 1) {
         api.value = getTenantUserList;
+        newColumns = columns.filter((col) => col.dataIndex !== "status");
       } else {
         api.value = getUserList;
+        newColumns = columns;
       }
       const [registerTable, { reload }] = useTable({
         title: "账号列表",
         api: api,
-        columns,
+        columns: newColumns,
         formConfig: {
           name: "search_form_item",
           labelWidth: 60,
@@ -131,10 +154,33 @@
         openPwdModal(true, { userId: id });
       }
 
+      function handleSelectAccount() {
+        openSelectModal(true);
+      }
+
       function handleSuccess() {
         reload();
       }
 
+      function handleRemoveUser(record: SsoUser) {
+        if (searchInfo.orgId == undefined || searchInfo.orgId == "") {
+          if (record.orgIds) {
+            if (record.orgIds.length > 1) {
+              createMessage.error("错误:请选择组织后再删除");
+              return;
+            }
+            deleteUserOrg({ userId: record.id, orgId: record.orgIds[0] }).then(() => {
+              handleSuccess();
+            });
+            return;
+          }
+          createMessage.error("错误:请选择组织后再删除");
+          return;
+        }
+        deleteUserOrg({ userId: record.id, orgId: searchInfo.orgId }).then(() => {
+          handleSuccess();
+        });
+      }
       function handleSelect(orgId = "") {
         searchInfo.orgId = orgId;
         reload();
@@ -155,7 +201,11 @@
         registerPwdModal,
         openPwdModal,
         handleChangePwd,
-        prefixCls
+        prefixCls,
+        hasTenant,
+        registerSelectModal,
+        handleSelectAccount,
+        handleRemoveUser
       };
     }
   };
